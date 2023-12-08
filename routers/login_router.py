44e31c00628b6a7c52f7d01dfb6de727084d7fb3
stiliamm <redemption.records@icloud.com>
2023-12-08 15:47:40 +0200

    feature: login

---
+++ routers/login_router.py
@@ -0,0 +1,23 @@
+import os
+from fastapi import APIRouter, HTTPException, status
+from common.models.login import Login
+from services.login_service import try_login
+from utils.auth import create_access_token
+
+login_router = APIRouter('/login', tags=['Login'])
+token_expiration = os.getenv('ACCESS_TOKEN_EXPIRATION')
+
+
+@login_router.post('/user', tags=['Login'])
+def login_user(login_data: Login):
+    user = try_login(login_data.username, login_data.password)
+
+    if user:
+        token = create_access_token(user, token_expiration)
+        return {"token": token}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
